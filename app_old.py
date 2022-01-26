9331bc5fd27e9e9c77d8303ba9127f1d4bad7f24
Anna Anttila <annaanttila@Annas-MacBook-Air.local>
2022-01-26 10:40:45 -0500

    all files

--- app_old.py
+++ app_old.py
@@ -18,7 +18,6 @@ external_stylesheets = [
     },
 ]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
 app.title = "deCrypto: Understand Your Cryptocurrencies!"
 
 app.layout = html.Div(
